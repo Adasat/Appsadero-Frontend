4cf23154f2e14e9a357df5203aab63479def461d
Diana <dianatabraue@gmail.com>
2023-06-15 11:27:54 +0100

    my 1st commit

--- Frontend/src/Layout/Layout.jsx
+++ Frontend/src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../Components/Header/Header'
+import Footer from '../Components/Footer/Footer'
 
 function Root() {
   return (
     <div>
         <Header/>
         <Outlet/>
+        <Footer/>
     </div>
   )
 }
--- Frontend/src/Router/router.jsx
+++ Frontend/src/Router/router.jsx
@@ -1,13 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import Root from "../Layout";
-import NotFound from "../Pages/NotFound/NotFound";
-
+import { createBrowserRouter } from 'react-router-dom'
+import Root from '../Layout'
+import NotFound from '../Pages/NotFound/NotFound'
+import Dashboard from '../Pages/Dashboard/Dashboard'
 
 export const router = createBrowserRouter([
-    {
-        path: '/', 
-        element: <Root/>,
-        errorElement: <NotFound/>, 
-    }
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+        { path: '/dashboard', element: <Dashboard /> },
+    ],
+  },
 ])
-
