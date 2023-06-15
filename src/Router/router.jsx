215033256eb1f1a4f2e36fbcec7678496e20f318
Diana <dianatabraue@gmail.com>
2023-06-15 16:40:52 +0100

    footer done

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Root from '../Layout/Layout'
+//import LayoutWelcome from '../Layout/LayoutWelcome/LayoutWelcome'
+import LayoutMain from '../Layout/LayoutMain/LayoutMain'
 import NotFound from '../Pages/NotFound/NotFound'
 import Dashboard from '../Pages/Dashboard/Dashboard'
 
@@ -7,7 +8,7 @@ import Dashboard from '../Pages/Dashboard/Dashboard'
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root/>,
+    element: <LayoutMain/>,
     errorElement: <NotFound />,
     children: [
         { path: '/dashboard', element: <Dashboard /> },
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './fonts/roboto'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './Router/index.jsx'
+import { router } from './Router/router'
+import {customTheme} from './Theme/Theme.jsx'
 import './index.css'
-import { CssBaseline, ThemeProvider } from '@mui/material'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <ThemeProvider customTheme={customTheme}/>
-  </React.StrictMode>,
+      <ThemeProvider theme={customTheme}> 
+         <CssBaseline/>
+          <RouterProvider router={router}/>        
+       </ThemeProvider> 
+  </React.StrictMode>
 )
