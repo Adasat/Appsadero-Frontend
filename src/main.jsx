ece50225edc045b5090373d3a6c96d1fe6cd1f00
Adasat <pbonbol@gmail.com>
2023-06-15 11:58:54 +0100

    one space

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './fonts/roboto'
-
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/index.jsx'
 
