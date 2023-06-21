4932348eb9a58c51b3f1b4279461bc2ffc4143ae
Adasat <pbonbol@gmail.com>
2023-06-22 00:50:03 +0100

    Changes done

--- src/Components/CardAsadero/CardAsadero.jsx
+++ src/Components/CardAsadero/CardAsadero.jsx
@@ -86,132 +86,135 @@ function CardAsadero({ bbq, owner }) {
     openVSclosed(bbq.confirmation_date)
   }, [currentDay, rejected])
 
+  /////////////////////////////////////////////////////////////////////////////////////
 
+  const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />
+  })
 
-/////////////////////////////////////////////////////////////////////////////////////
+  function FullScreenDialog() {
+    const [open, setOpen] = useState(false)
 
-const Transition = forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+    const handleClickOpen = () => {
+      setOpen(true)
+    }
 
-function FullScreenDialog() {
-  const [open, setOpen] = useState(false);
+    const handleClose = () => {
+      setOpen(false)
+      location.reload()
+    }
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const totalPrice = (param) => {
+      let sum = 0
+      sum += param
+      return sum
+    }
 
-  const handleClose = () => {
-    setOpen(false);
-    location.reload()
-  };
-  
-  const totalPrice = (param) => {
-    let sum = 0
-    sum += param
-    return sum
+    open
+    return (
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Ver más detalles
+        </Button>
+        <Dialog
+          fullScreen
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Transition}
+        >
+          <AppBar sx={{ position: 'relative' }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleClose}
+                aria-label="close"
+              >
+                <Close />
+              </IconButton>
+              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                {bbq.name}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <Grid container spacing={5} justifyContent={'center'} margin={1}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper elevation={24}>
+                <Card>
+                  <CardHeader title={bbq.name} sx={{ fontWeight: 'bold' }} />
+                  <CardMedia
+                    component="img"
+                    height="194"
+                    image="https://source.unsplash.com/random?bbq,rotisserie"
+                    alt="Grill, barbacue"
+                  />
+                  <CardContent>
+                    <Typography textAlign={'center'} variant="h4"></Typography>
+                    <Divider sx={{ marginBottom: '10px' }} />
+
+                    <Typography variant="h7">
+                      Descripción: {bbq.description}
+                    </Typography>
+                    <Typography variant="h6">
+                      Fecha: {formatDate(bbq.date_time)}
+                    </Typography>
+                    <Typography variant="h6">
+                      Hora de inicio: 11:00 horas{' '}
+                    </Typography>
+                    <Typography variant="h7">
+                      Duración: {bbq.duration} horas
+                    </Typography>
+                    <Divider sx={{ marginTop: '20px' }} />
+                    <Typography>
+                      Último día de confirmación:{' '}
+                      {formatDate(bbq.confirmation_date)}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography variant="h5" align="center">
+                Menú
+              </Typography>
+              <List>
+                {shoppingCart && shoppingCart.length ? (
+                  shoppingCart.map((prod) => (
+                    <ListItem key={prod.id}>{prod.name}</ListItem>
+                  ))
+                ) : (
+                  <Typography variant="body1" align="center">
+                    No hay productos en tu lista
+                  </Typography>
+                )}
+              </List>
+            </Grid>
+            <Grid item xs={12} sm={12} md={3}>
+              <List>
+                <Typography variant="h5" textAlign="center">
+                  Lista de invitados
+                </Typography>
+                <Divider sx={{ marginBottom: '10px' }} />
+
+                {users
+                  .filter(
+                    (el) => el.status === 'pending' || el.status === 'confirmed'
+                  )
+                  .map((el) => (
+                    <Typography variant="h6" key={el.id} textAlign="center">
+                      {el.first_name}
+                    </Typography>
+                  ))}
+              </List>
+            </Grid>
+          </Grid>
+        </Dialog>
+      </div>
+    )
   }
 
-  (open)
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Ver más detalles
-      </Button>
-      <Dialog
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Transition}
-      >
-        <AppBar sx={{ position: 'relative' }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="close"
-            >
-              <Close />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              {bbq.name}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-
-        <Grid container spacing={5} justifyContent={'center'} margin={1}>
-
-          <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={24}>
-              <Card>
-                <CardHeader title={bbq.name} sx={{fontWeight:'bold'}}/>
-                <CardMedia component="img"
-        height="194"
-        image="https://source.unsplash.com/random?bbq,rotisserie"
-        alt="Grill, barbacue"/>
-                <CardContent>
-
-                
-              <Typography textAlign={'center'} variant="h4">
-                
-              </Typography>
-              <Divider sx={{ marginBottom: '10px' }} />
-
-              <Typography variant="h7">
-                Descripción: {bbq.description}
-              </Typography>
-              <Typography variant="h6">
-                Fecha: {formatDate(bbq.date_time)}
-              </Typography>
-              <Typography variant="h6">Hora de inicio: 11:00 horas </Typography>
-              <Typography variant="h7">Duración: {bbq.duration} horas</Typography>
-              <Divider sx={{ marginTop: '20px' }} />
-              <Typography>Último día de confirmación: {formatDate(bbq.confirmation_date)}</Typography>
-              </CardContent>
-              </Card>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-  <Typography variant="h5" align="center">
-    Menú
-  </Typography>
-  <List>
-    {shoppingCart && shoppingCart.length ? (
-      shoppingCart.map((prod) => (
-        <ListItem key={prod.id}>{prod.name}</ListItem>
-      ))
-    ) : (
-      <Typography variant="body1" align="center">
-        No hay productos en tu lista
-      </Typography>
-    )}
-  </List>
-</Grid>
-          <Grid item xs={12} sm={12} md={3}>
-            <List>
-              <Typography variant="h5" textAlign="center">
-                Lista de invitados
-              </Typography>
-              <Divider sx={{ marginBottom: '10px' }} />
-
-              {users.map((el) => (
-                <Typography variant="h6" key={el.id} textAlign="center">
-                  {el.first_name}
-                </Typography>
-              ))}
-            </List>
-          </Grid>
-        </Grid>
-      </Dialog>
-    </div>
-  )
-}
-
-
-/////////////////////////////////////////////////////////////////////////////////////
-
-
+  /////////////////////////////////////////////////////////////////////////////////////
 
   return (
     <Card>
@@ -222,7 +225,13 @@ function FullScreenDialog() {
             el.first_name === localStorage.getItem('first_name')
         )
         .map((el) => (
-          <Chip key={el.id} label="NEW" color="error" sx={{ m: 1 }} onClick={handleNew}></Chip>
+          <Chip
+            key={el.id}
+            label="NEW"
+            color="error"
+            sx={{ m: 1 }}
+            onClick={handleNew}
+          ></Chip>
         ))}
       <Link className="links" /* to={`/home/manageAsadero/${bbq.id}`} */>
         <CardHeader
@@ -233,7 +242,11 @@ function FullScreenDialog() {
 
       {users && users !== undefined
         ? users
-            .filter((el) => el.status === 'pending' && el.first_name === 'Ciru')
+            .filter(
+              (el) =>
+                el.status === 'pending' &&
+                el.nickname === localStorage.getItem('nickname')
+            )
             .map((el) => <Chip key={el.id} label="NEW" color="error"></Chip>)
         : null}
       <Divider sx={{ marginBottom: '10px' }} />
@@ -250,23 +263,30 @@ function FullScreenDialog() {
             <Typography variant="body">Invitados</Typography>
           </AccordionSummary>
           <div>
-            {users.filter((el) => el.status !== 'rejected').map((el) => (
-              <Typography variant="body1" key={el.id}>
-                {el.first_name} {(owner === true && el.status !== 'rejected') && `- ${el.status}`}{' '}
-                {el.isChef === true && ' -> Chef'}
-              </Typography>
-            ))}
+            {users
+              .filter((el) => el.status !== 'rejected')
+              .map((el) => (
+                <Typography variant="body1" key={el.id}>
+                  {el.first_name}{' '}
+                  {owner === true &&
+                    el.status !== 'rejected' &&
+                    `- ${el.status}`}{' '}
+                  {el.isChef === true && ' -> Chef'}
+                </Typography>
+              ))}
           </div>
           <Divider sx={{ marginBottom: '10px' }} />
           <Typography variant="body1" textAlign="right" alignContent="center">
             {(() => {
-      const userFiltered = users.filter((el) => el.status !== 'rejected');
-      return userFiltered.length > 0
-        ? userFiltered.length > 1
-          ? `${userFiltered.length} personas`
-          : `${userFiltered.length} persona`
-        : 'No hay invitados aún.';
-    })()}
+              const userFiltered = users.filter(
+                (el) => el.status !== 'rejected'
+              )
+              return userFiltered.length > 0
+                ? userFiltered.length > 1
+                  ? `${userFiltered.length} personas`
+                  : `${userFiltered.length} persona`
+                : 'No hay invitados aún.'
+            })()}
           </Typography>
         </Accordion>
         {owner === true && (
