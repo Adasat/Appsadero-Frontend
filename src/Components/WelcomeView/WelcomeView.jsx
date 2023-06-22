c655ae365f2411630491bad789400bce27d348ec
Adasat <pbonbol@gmail.com>
2023-06-22 23:51:05 +0100

    Fixed the invitations

--- src/Components/WelcomeView/WelcomeView.jsx
+++ src/Components/WelcomeView/WelcomeView.jsx
@@ -9,7 +9,7 @@ import Slice4 from '../../assets/slice4.png'
 function WelcomeView() {
   return (
     <Box width={'99vw'} height={'50%'}>
-        <Carousel width={'100%'} autoPlay={true} showArrows dynamicHeight infiniteLoop interval={2000} useKeyboardArrows={true} showThumbs={false} >
+        <Carousel width={'100%'} autoPlay={true} showArrows dynamicHeight infiniteLoop interval={5000} useKeyboardArrows={true} showThumbs={false} >
         <div>
         <img alt="" src={Slice1}/>
         </div>
